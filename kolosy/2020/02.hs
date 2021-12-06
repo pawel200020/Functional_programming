e5c9459fe858c1ee35c1fab4a5569d0fff8be4d1
Paweł Pyciński <pawel200020@gmail.com>
2021-12-06 02:33:42 +0100

    kolos 2020 fixes

--- kolosy/2020/02.hs
+++ kolosy/2020/02.hs
@@ -1,8 +1,10 @@
 data Tree a = Empty | Node a (Tree a) (Tree a)
 
-isExist:: Eq a => a -> Tree a -> Bool
-isExist _ Empty = False
-isExist  x (Node a left right) = if(x== a) then True else isExist x left 
--- findPath :: Eq a => a -> Tree a -> [a]
--- findPath _ Empty  = []
--- findPath x Node a (Tree a) (Tree a) = if 
+hasElement:: Eq a => a -> Tree a -> Bool
+hasElement _ Empty = False 
+hasElement x (Node a Empty Empty) = if x==a then True  else False 
+hasElement x (Node a l r) = if x==a then True else (hasElement x l) || (hasElement x r)
+
+findPath :: Eq a => a -> Tree a -> [a]
+findPath _ Empty = []
+findPath x (Node a l r) = if hasElement x (Node a l r) then [a]++(findPath x l) ++ (findPath x r)else (findPath x l) ++ (findPath x r)
--- kolosy/2020/03.hs
+++ kolosy/2020/03.hs
@@ -1,25 +1,13 @@
+hasNumber:: Integer ->[Integer]->Bool 
+hasNumber _ [] =False
+hasNumber number list = if length (filter(==number) list) > 0 then True else False
 
--- doNPozycji:: Int-> [a] -> [a]
--- doNPozycji 0 (x:xs) = [x]
--- doNPozycji n (x:xs) = x:doNPozycji (n-1) xs
+hasAllLoverVals:: [Integer]->Integer->Bool
+hasAllLoverVals array val=  if length (filter (==False) (map(\x->hasNumber x array) [1..(val-1)])) > 0 then False else True
 
--- listOfLoverVals:: Int -> [Int]
--- listOfLoverVals  0 = []
--- listOfLoverVals x = takeWhile (<x) [1..]
+isClosing:: Integer -> [Integer]->Bool 
+isClosing index array |toInteger index +1 < array!! fromIntegral  index  = False 
+                      |otherwise = hasAllLoverVals (take (fromIntegral  index) array) (array!! fromIntegral index)
 
--- hasValue:: [Int]-> Int-> Bool
--- hasValue [] _ = False 
--- hasValue (x:xs) v = if(x==v) then True else hasValue xs v
-
--- hasAllLoverVals:: [Int]->Int-> [Bool]
--- hasAllLoverVals [] _ = [True]
--- hasAllLoverVals list n = if(length list /= n-1) then [False] else map(\x -> hasValue list x) [1..n-1]
-
--- hasAllLoverVals2 :: [Bool]-> Bool 
--- hasAllLoverVals2 list =  length (filter (==True) list) == length list
-
-
--- cp :: [Int] -> [Int]
--- cp [] = []
--- cp list = filter (\x->hasAllLoverVals2(hasAllLoverVals take  x)) list
- 
+closingIndexes:: [Integer]->[Integer]
+closingIndexes array = filter (\x -> isClosing (x-1) array) [1..(toInteger(length array) )]
