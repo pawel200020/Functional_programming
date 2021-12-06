e3caec46e4bb36fbc0f3724b99eae3dfb2a57fb5
Paweł Pyciński <pawel200020@gmail.com>
2021-12-06 02:38:50 +0100

    code refactory

--- kolosy/2020/02.hs
+++ kolosy/2020/02.hs
@@ -1,8 +1,8 @@
 data Tree a = Empty | Node a (Tree a) (Tree a)
 
 hasElement:: Eq a => a -> Tree a -> Bool
-hasElement _ Empty = False 
-hasElement x (Node a Empty Empty) = if x==a then True  else False 
+hasElement _ Empty = False
+hasElement x (Node a Empty Empty) = x==a
 hasElement x (Node a l r) = if x==a then True else (hasElement x l) || (hasElement x r)
 
 findPath :: Eq a => a -> Tree a -> [a]
--- kolosy/2020/03.hs
+++ kolosy/2020/03.hs
@@ -2,12 +2,12 @@ hasNumber:: Integer ->[Integer]->Bool
 hasNumber _ [] =False
 hasNumber number list = if length (filter(==number) list) > 0 then True else False
 
-hasAllLoverVals:: [Integer]->Integer->Bool
-hasAllLoverVals array val=  if length (filter (==False) (map(\x->hasNumber x array) [1..(val-1)])) > 0 then False else True
+hasAllLowerVals:: [Integer]->Integer->Bool
+hasAllLowerVals array val=  if length (filter (==False) (map(\x->hasNumber x array) [1..(val-1)])) > 0 then False else True
 
 isClosing:: Integer -> [Integer]->Bool 
 isClosing index array |toInteger index +1 < array!! fromIntegral  index  = False 
-                      |otherwise = hasAllLoverVals (take (fromIntegral  index) array) (array!! fromIntegral index)
+                      |otherwise = hasAllLowerVals (take (fromIntegral  index) array) (array!! fromIntegral index)
 
 closingIndexes:: [Integer]->[Integer]
 closingIndexes array = filter (\x -> isClosing (x-1) array) [1..(toInteger(length array) )]
